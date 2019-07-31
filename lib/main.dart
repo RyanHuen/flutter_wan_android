be6a770372fb47af1659c2bc94a398e16309bc8a
MuXi <muxi@ibantang.com>
2019-07-31 20:02:50 +0800

    init splash页面

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'splash_widget.dart';
+
+void main() {
+  runApp(MyApp());
+  if (Platform.isAndroid) {
+//    设置Android头部的导航栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Wan Android',
+      home: SplashWidget(),
+    );
+  }
+}
---
+++ lib/splash_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Container(
+        child: Center(
+          child: Image(
+            fit: BoxFit.fill,
+            width: 320,
+            height: 240,
+            image: AssetImage('images/logo.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
